1464c91e10534a71a554b89227f986a63038cf81
AKOMADEIT | Asante Inc. <okyereasante08@gmail.com>
2026-06-26 15:27:44 +0000

    Update bioml.py

--- bioml.py
+++ bioml.py
@@ -1,3 +1,11 @@
-from bioml import gc_content
-for s in ["ATGC", "GGGGCCCC", "ATATATAT"]:
-    print(s, "->", round(gc_content(s), 1), "%")
+# bioml.py
+
+def gc_content(sequence: str) -> float:
+    """Calculate the GC content of a DNA sequence as a percentage."""
+    s = str(sequence).upper()
+    if not s:
+        return 0.0
+    
+    # Count G and C bases
+    gc_count = s.count("G") + s.count("C")
+    return (gc_count / len(s)) * 100
